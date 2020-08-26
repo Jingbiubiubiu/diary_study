0689b076c763281344bcc94f30fc05aa6eb91ed0
Zicong Ma <mazicong@gmail.com>
2020-08-26 11:27:09 +0100

    update url

--- constants/URL.js
+++ constants/URL.js
@@ -1,4 +1,4 @@
 export default {
-  address: 'http://127.0.0.1:3000/',
-  //   address: 'https://7f3eb59f6b21.ngrok.io/',
+    // address: 'http://127.0.0.1:3000/'
+    address: 'https://0a9cf7ef32e6.ngrok.io/'
 };
